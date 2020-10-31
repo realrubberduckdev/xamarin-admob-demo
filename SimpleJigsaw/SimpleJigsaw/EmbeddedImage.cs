82d0ab43420abdb6ef64cb0fdc77eb48d7454d4f
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2020-10-31 16:46:40 +0000

    Update for ease of usage.

---
+++ SimpleJigsaw/SimpleJigsaw/EmbeddedImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleJigsaw
+{
+    public class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourceId
+        {
+            get;
+            set;
+        }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrWhiteSpace(ResourceId)) return null;
+            return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
---
+++ SimpleJigsaw/SimpleJigsaw/Tile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xamarin.Forms;
+
+namespace SimpleJigsaw
+{
+    public class Tile
+    {
+        public Tile(int row, int col)
+        {
+            Row = row;
+            Col = col;
+            TileView = new ContentView
+            {
+                Padding = new Thickness(1),
+
+                // Get the bitmap for each tile 
+                Content = new Image
+                {
+                    Source = ImageSource.FromResource($"SimpleJigsaw.Images.row-{row + 1}-col-{col + 1}.jpg")
+                }
+            };
+
+            // Add TileView to dictionary for obtaining Tile from TileView
+            Dictionary.Add(TileView, this);
+        }
+
+        public static Dictionary<View, Tile> Dictionary { get; } = new Dictionary<View, Tile>();
+
+        public int Row { set; get; }
+
+        public int Col { set; get; }
+
+        public View TileView { private set; get; }
+    }
+}
